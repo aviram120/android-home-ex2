dc134282f5a4ef1b805b5fdadd66a7c6d2dc7f3e
aviram120 <aviram1202@gmail.com>
2015-12-16 22:29:01 +0200

    hendle with number input form usrer
    is setting page

--- ex2-home/app/src/main/java/com/example/aviram/ex2_home/setting.java
+++ ex2-home/app/src/main/java/com/example/aviram/ex2_home/setting.java
@@ -32,28 +32,48 @@ public class setting extends Activity implements View.OnClickListener {
         boolean flag=true;
         if (v.getId()==R.id.buttonSave)
         {
-            int levelNum=Integer.parseInt(etLevelNum.getText().toString());//get the date form the field
-            if ((levelNum>0)&(levelNum<11))//check if the user add ok number
+            String stLevelNum=etLevelNum.getText().toString();
+
+            if (!stLevelNum.contains(".")&&(!stLevelNum.isEmpty()))
             {
-                editor.putInt("level",levelNum);//put the date in sharedPref
-                editor.apply();
+                int levelNum=Integer.parseInt(stLevelNum);//get the date form the field
+                if ((levelNum>0)&(levelNum<11))//check if the user add ok number
+                {
+                    editor.putInt("level",levelNum);//put the date in sharedPref
+                    editor.apply();
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), "Level must be between 1-10", Toast.LENGTH_LONG).show();
+                    flag=false;
+                }
             }
             else
             {
-                Toast.makeText(getApplicationContext(), "Level must be between 1-10", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Level must be between 1-10 \nand Integer number", Toast.LENGTH_LONG).show();
                 flag=false;
             }
 
 
-            int complexNum=Integer.parseInt(etComplexNum.getText().toString());//get the date form the field
-            if ((complexNum>-1)&(complexNum<5))//check if the user add ok number
+            String stComplexNum=etComplexNum.getText().toString();
+            if (!stComplexNum.contains(".")&&(!stComplexNum.isEmpty()))
             {
-                editor.putInt("complexity",complexNum);//put the date in sharedPref
-                editor.apply();
+
+                int complexNum=Integer.parseInt(stComplexNum);//get the date form the field
+                if ((complexNum>-1)&(complexNum<5))//check if the user add ok number
+                {
+                    editor.putInt("complexity",complexNum);//put the date in sharedPref
+                    editor.apply();
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), "complexity must be between 0-4", Toast.LENGTH_LONG).show();
+                    flag=false;
+                }
             }
             else
             {
-                Toast.makeText(getApplicationContext(), "complexity must be between 0-4", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "complexity must be between 0-4 \nand Integer number", Toast.LENGTH_LONG).show();
                 flag=false;
             }
 
@@ -62,6 +82,7 @@ public class setting extends Activity implements View.OnClickListener {
                 Intent Inetent = new Intent(this,MainActivity.class);
                 startActivity(Inetent);
             }
+
 
         }
         Log.i("aviramLog", "level:" + sharedPref.getInt("level", 100));
